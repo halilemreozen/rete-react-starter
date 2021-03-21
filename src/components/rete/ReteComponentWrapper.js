3a83d6dabdda76fef28906ec62daf3bfa4c9cd09
Halil Emre ÖZEN <halilemreozen@users.noreply.github.com>
2021-03-21 23:03:30 +0300

    Add node component wrapper

--- src/components/rete/ReteComponentWrapper.js
+++ src/components/rete/ReteComponentWrapper.js
@@ -1,13 +1,26 @@
 import Rete from "rete";
+import { nanoid } from 'nanoid';
+import { CustomReteNode } from "./CustomReteNode";
+
 
 export class ReteComponentWrapper extends Rete.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.data.render = 'react';
+        this.data.component = CustomReteNode;
+        this.data.props = {};
+    }
+
     builder(node) {
-        throw 'ReteComponentWrapper::builder not implemented'
+        node.data.identifier = `${node.name}_${nanoid(11)}`
+            .replace(/\s/g, '_')
+            .replace(/-/g, '_');
+        return node;
     }
 
     worker(node, inputs, outputs) {
-        throw 'ReteComponentWrapper::builder not implemented'
     }
 
 }
